f61392196e141d2729d9c5c4245909eb295f42f0
kei95 <kei0613shine.star@gmail.com>
2022-11-27 14:13:54 -0800

    initial commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,62 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { useEffect, useState } from "react";
 
-function App() {
+interface Photo {
+  albumId: number;
+  id: number;
+  title: string;
+  url: string;
+  thumbnailUrl: string;
+}
+
+/**
+ * function to retrieve my following users in github
+ */
+async function fetchFollowers(): Promise<Photo[] | undefined> {
+  try {
+    const res = await fetch(`https://jsonplaceholder.typicode.com/photos`);
+    const isResponseSuccessful = res.status < 300 && res.status > 199;
+
+    if (isResponseSuccessful) {
+      const jsonRes = await res.json();
+      return jsonRes;
+    }
+  } catch (error) {
+    console.error("error!", error);
+  }
+
+  // case of failure/error
+  return undefined;
+}
+
+export function App() {
+  const [photos, setPhotos] = useState<Photo[] | undefined>(undefined);
+
+  useEffect(() => {
+    async function getFollowers() {
+      const res = await fetchFollowers();
+      setPhotos(res);
+    }
+
+    getFollowers();
+  }, []);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container">
+      <h1 className="title">Welcome to Create React App!</h1>
+      {photos ? (
+        <ul>
+          {photos.map((photo, idx) => (
+            <a href={photo.url} key={`follower_${photo.thumbnailUrl}_${idx}`}>
+              <li className="card">
+                <p>{photo.title}</p>
+                {photo.url}
+              </li>
+            </a>
+          ))}
+        </ul>
+      ) : (
+        <h1>Loading...</h1>
+      )}
     </div>
   );
 }
-
-export default App;
